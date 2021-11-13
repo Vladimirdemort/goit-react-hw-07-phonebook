6de3e39505daa634ddb71b97ec458d4fbe6f4b32
Volodymyr Dvorskyi <feowurd@gmail.com>
2021-11-13 22:13:41 +0200

    hw 3.1

--- src/App.js
+++ src/App.js
@@ -11,12 +11,7 @@ console.dir(uuidv4());
 
 class App extends React.Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -35,6 +30,7 @@ class App extends React.Component {
     this.setState({
       contacts: setContact,
     });
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
   };
 
   nameFilter = e => {
@@ -58,6 +54,21 @@ class App extends React.Component {
       contacts: prev.contacts.filter(contact => contact.id !== e),
     }));
   };
+
+  componentDidMount() {
+    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+    this.setState({
+      contacts: parsedContacts,
+    });
+    console.log(parsedContacts);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevState);
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const filterSearch = this.filteredList();
